cab1979bd136dd038bf831c916eccf86f5bfb553
FBI00269 <syk531@pionnet.co.kr>
2021-01-29 16:52:54 +0900

    hash04

--- src/programmers/hash04.java
+++ src/programmers/hash04.java
@@ -1,7 +1,13 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class hash04 {
 	public static void main(String[] args) {
@@ -24,7 +30,7 @@ public class hash04 {
 					
 			if(musicMap.containsKey(genresName)) {
 				Music music = musicMap.get(genresName);
-				music.totalPlayCnt =+ playCnt;
+				music.totalPlayCnt = music.totalPlayCnt + playCnt;
 				music.play.put(i, playCnt);
 			} else {
 				Music music = new Music(genresName, playCnt, i);
@@ -32,20 +38,71 @@ public class hash04 {
 			}
 		}
 		
-        int[] answer = {};
+		Music[] musicArr = new Music[musicMap.entrySet().size()];
+		int musicArrIndex=0;
+		for(Map.Entry<String, Music> entry : musicMap.entrySet()) {
+			musicArr[musicArrIndex] = entry.getValue();
+			musicArrIndex++;
+		}
+		
+		Arrays.sort(musicArr, Collections.reverseOrder());
+		ArrayList<Integer> answerList = new ArrayList<>();
+		
+		for(int i=0; i<musicArr.length; i++) {
+			for(int j=0; j<musicArr[i].play.size(); j++) {
+				//젇렬
+				List<Entry<Integer, Integer>> playList = new ArrayList<Entry<Integer, Integer>>(musicArr[i].play.entrySet());
+				
+				Collections.sort(playList, new Comparator<Entry<Integer, Integer>>() {
+					@Override
+					public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+						return o1.getValue().compareTo(o2.getValue());
+					}
+				});
+				
+				for(int k=0; k<playList.size(); k++) {
+					answerList.add(playList.get(k).getKey());
+					
+					if(k==1) {
+						break;
+					}
+				}
+			}
+		}
+
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++) {
+        	answer[i] = answerList.get(i);
+        }
         return answer;
     }
 
 }
 
-class Music {
-	String genresName;
-	int totalPlayCnt;
-	Map<Integer, Integer> play; //유니크번호, 플래이횟수
+class Music implements Comparable {
+	String genresName = "";
+	int totalPlayCnt = 0;
+	Map<Integer, Integer> play = new HashMap<Integer, Integer>(); //고유번호, 플래이횟수
 	
 	public Music(String genresName, int playCnt, int uniqueNum) {
 		this.genresName = genresName;
 		this.totalPlayCnt =+ playCnt;
 		this.play.put(uniqueNum, playCnt);
 	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		Music music = (Music) o;
+
+		if(this.totalPlayCnt < music.totalPlayCnt) {
+			return -1;
+		} else if(this.totalPlayCnt == music.totalPlayCnt) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 }
+
+
