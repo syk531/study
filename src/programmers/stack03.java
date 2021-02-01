12da09c415daa0aedf87d12dcc15e4b4a35cc00c
FBI00269 <syk531@pionnet.co.kr>
2021-02-01 16:53:09 +0900

    stack03 init

---
+++ src/programmers/stack03.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class stack03 {
+	public static void main(String args[]) {
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weights = {7,4,5,6};
+		
+		solution(bridge_length, weight, truck_weights);
+	}
+	
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
