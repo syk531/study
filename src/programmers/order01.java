9a6123cbc38e60e5ff462543c2c777908807fea3
FBI00269 <syk531@pionnet.co.kr>
2021-02-03 09:07:33 +0900

    order01

---
+++ src/programmers/order01.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class order01 {
+	public static void main(String args[]) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		
+		solution(array, commands);
+	}
+	
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		int[] command = new int[3];
+		
+		for(int i=0; i<commands.length; i++) {
+			command = commands[i]; // {2, 5, 3}
+			
+			int[] splitedArray = Arrays.copyOfRange(array, command[0]-1, command[1]);
+			Arrays.sort(splitedArray);
+			answer[i] = splitedArray[command[2]-1];
+		}
+		
+		
+        return answer;
+    }
+}
