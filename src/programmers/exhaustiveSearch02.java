bf9341066e35670402f30ad895220a664540a8c6
FBI00269 <syk531@pionnet.co.kr>
2021-02-04 10:05:06 +0900

    exhaustiveSearch02 런타임에러

--- src/programmers/exhaustiveSearch02.java
+++ src/programmers/exhaustiveSearch02.java
@@ -7,36 +7,56 @@ import java.util.Map;
 
 public class exhaustiveSearch02 {
 	public static void main(String args[]) {
-		String numbers = "17";
+		String numbers = "011";
 		solution(numbers);
 	}
 	
 	public static int solution(String numbers) {
-		/*
-		  	numbers는 길이 1 이상 7 이하인 문자열입니다.
-			numbers는 0~9까지 숫자만으로 이루어져 있습니다.
-			013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
-		 */
-		
-		Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>(); //자리수, 숫자
-		List<Integer> list = new ArrayList<Integer>();
+		Map<Integer, List<Number>> map = new HashMap<Integer, List<Number>>(); //자리수, 넘버클래스리스트
 		int digit = 1; //자리수
-		int number = 0;
+		String substrNumber = "";
+		String concatNumber = "";
 		
 		//조합가능 숫자 만들기
-		while(digit == numbers.length()) {
+		while(digit <= numbers.length()) {
 			for(int i=0; i<numbers.length(); i++) {
-				number = Integer.parseInt(numbers.substring(i, i+1));
-				
-				if(map.containsKey(digit)) {
-					list = map.get(digit);
-				}
+				substrNumber = numbers.substring(i, i+1);
+
+				List<Number> numberList = new ArrayList<Number>();
+				List<Integer> indexList = new ArrayList<Integer>();
 				
 				if(digit == 1) {
-					list.add(number);
-					map.put(digit, list);
+					indexList.add(i);
+					Number number = new Number(indexList, substrNumber);
+					
+					if(!map.containsKey(digit)) {
+						numberList.add(number);
+						map.put(digit, numberList);
+					} else {
+						numberList = map.get(digit);
+						numberList.add(number);
+					}
 				} else {
-					list = map.get(digit-1);
+					List<Number> beforeNumberList = map.get(digit-1); //자리수 하나 작은 넘버클래스리스트
+					
+					for(Number numberObj : beforeNumberList) {
+						indexList = numberObj.indexList;
+						concatNumber = numberObj.number; 
+						
+						if(!indexList.contains(i)) { //연결안한 인덱스일 경우에 연결해주기
+							indexList.add(i);
+							
+							Number number = new Number(indexList, concatNumber + substrNumber);
+							
+							if(!map.containsKey(digit)) {
+								numberList.add(number);
+								map.put(digit, numberList);
+							} else {
+								numberList = map.get(digit);
+								numberList.add(number);
+							}
+						}
+					}
 				}
 			}
 			
@@ -44,8 +64,43 @@ public class exhaustiveSearch02 {
 		}
 		
 		//조합가능 숫자 중에서 소수 구하기
-		
-        int answer = 0;
+		int answer = 0;
+		int num = 0;
+		List<Number> numberList = new ArrayList<Number>();
+		for(Integer key : map.keySet()) {
+			numberList = map.get(key);
+			
+			for(Number number : numberList) {
+				//소수인지 체크
+				boolean isPrime = false;
+				num = Integer.parseInt(number.number); 
+				if(num > 0) {
+					for(int i=2; i<num/2; i++) {
+						if(num % i == 0) {
+							isPrime = false;
+							break;
+						} else {
+							isPrime = true;
+						}
+					}
+				}
+				
+				if(isPrime) {
+					answer++;
+				}
+			}
+		}
+        
         return answer;
     }
+}
+
+class Number {
+	List<Integer> indexList = new ArrayList<Integer>();
+	String number = "";
+	
+	Number(List<Integer> indexList, String number) {
+		this.indexList = indexList;
+		this.number = number;
+	}
 }
