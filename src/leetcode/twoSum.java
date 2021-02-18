98e743b544e018ef057849685c7720d65c6f27bc
FBI00269 <syk531@pionnet.co.kr>
2021-02-18 09:26:33 +0900

    leetcode - tow sum

---
+++ src/leetcode/twoSum.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class twoSum {
+
+	public static void main(String args[]) {
+		int[] nums = {3,2,4};
+		int target = 6;
+		
+		twoSum(nums, target);
+	}
+	
+	public static int[] twoSum(int[] nums, int target) {
+        boolean answered = false;
+		int[] answer = new int[2];
+        for(int i=0; i<nums.length-1; i++) {
+        	if(answered) {
+        		break;
+        	}
+        	
+        	for(int j=i+1; j<nums.length; j++) {
+            	if(target == nums[i] + nums[j]) {
+            		answer[0] = i;
+            		answer[1] = j;
+            		answered = true;
+            		break;
+            	}
+        	}
+        }
+
+        return answer;
+    }
+}
